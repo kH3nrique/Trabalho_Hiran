1d19046a54f76fa81d2d5349c1b3a4a1de89d778
kH3nrique <92551677+kH3nrique@users.noreply.github.com>
2022-06-07 15:58:49 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -13,16 +13,17 @@ int main(){
 
     for (i = 0; i < 7; i++)
     {
-            valor[i].latitude = rand()%1000;
-            valor[i].longitude = rand()%1000;
-            valor[i].acidez = rand()%100;
-            valor[i].producao = rand()%100;
+            printf("Latitude: %d\n", valor[i].latitude = rand()%1000);
+            printf("Longitude: %d\n", valor[i].longitude = rand()%1000);
+            printf("Producao: %d\n", valor[i].producao = rand()%100);
+            printf("Acidez: %d\n", valor[i].acidez = rand()%100);
+            printf("----------------------------------------------------------------------\n");
     }
     int posicao;
     printf("Posicao(1 a 7): ");
         scanf("%d", &posicao);
-    printf("Valores da posicao %d\n", posicao);
-    printf("Latitude: %d\nLongitude: %d\nAcidez: %d\nProducao: %d\n", valor[posicao-1].latitude, valor[posicao-1].longitude, valor[posicao-1].acidez, valor[posicao-1].producao);
+    printf("Valores da posicao: %d\n", posicao);
+    printf("Latitude: %d\nLongitude: %d\nProducao: %d\nAcidez: %d\n", valor[posicao-1].latitude, valor[posicao-1].longitude, valor[posicao-1].producao, valor[posicao-1].acidez);
 
 system("pause");
 return 0;
