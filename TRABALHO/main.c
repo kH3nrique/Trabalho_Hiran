f351dfbe0fe073343c21c12fd78bc2790e073ae9
kH3nrique <92551677+kH3nrique@users.noreply.github.com>
2022-06-08 16:16:41 -0300

    Add files via upload

--- TRABALHO/main.c
+++ TRABALHO/main.c
@@ -5,7 +5,7 @@
 int main(){   
     int latitude[100], longitude[100], acidez[100], producao[100];
     int i;
-    
+    int melhor_acidez[100], melhor_producao[100];
     for (i = 0; i < 100; i++)
     {
         printf("Latitude: %d\t",  latitude[i] = rand()%1000);
@@ -13,8 +13,15 @@ int main(){
         printf("Producao: %d\t",  producao[i] = rand()%100);
         printf("Acidez: %d\n",  acidez[i] = rand()%100);
         printf("--------------------------------------------------------------------------------------------------------------------\n");
+        melhor_acidez[i] = acidez[i];
+        melhor_producao[i] = producao[i];
     }
-    int melhor_acidez[100], melhor_producao[100], maior = 0;
+    
+
+    for (i = 0; i < 100; i++)
+    {
+        
+    }
     
 
 system("pause");
