7d50d196a058b36e40ad0cbdcf78194a218ebf79
kH3nrique <92551677+kH3nrique@users.noreply.github.com>
2022-06-13 12:39:02 -0300

    Add files via upload

--- TRABALHO/main.c
+++ TRABALHO/main.c
@@ -66,6 +66,41 @@ int main(){
     }
     printf("\nMude a acidez e a producao: ");
         scanf("%d %d", &acidez[posicao_longitude_latitude], &producao[posicao_longitude_latitude]);
+    
+    for (i = 0; i < 5; i++)
+    {
+        melhor_acidez[i] = acidez[i];
+        melhor_producao[i] = producao[i];
+    }  
+    
+    for (i = 0; i < 5; i++)
+    {
+        for (j = i + 1; j < 5; j++)
+        {
+            if (melhor_acidez[i] < melhor_acidez[j])
+            {
+                auxiliar = melhor_acidez[i];
+                melhor_acidez[i] = melhor_acidez[j];
+                melhor_acidez[j] = auxiliar;
+                posicao_melhor_acidez[i] = j;
+            }
+            if (melhor_producao[i] < melhor_producao[j])
+            {
+                auxiliar = melhor_producao[i];
+                melhor_producao[i] = melhor_producao[j];
+                melhor_producao[j] = auxiliar;
+                posicao_melhor_producao[i] = j;
+            }
+        }
+    }
+
+    printf("\nLatitude: %d\nLongitude: %d\nAcidez do solo: %d\n", latitude[posicao_melhor_producao[posicao-1]], longitude[posicao_melhor_producao[posicao-1]], acidez[posicao_melhor_producao[posicao-1]]);
+    printf("Melhor producao: %d\n", melhor_producao[posicao-1]);
+
+    printf("\n");
+
+    printf("Latitude: %d\nLongitude: %d\nProducao: %d\n", latitude[posicao_melhor_acidez[posicao-1]], longitude[posicao_melhor_acidez[posicao-1]], producao[posicao_melhor_producao[posicao-1]]);
+    printf("Melhor acidez: %d\n", melhor_acidez[posicao-1]);
 system("pause");
 return 0;
 }
